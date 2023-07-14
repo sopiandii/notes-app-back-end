3860dfbe3ceb5856598c4e8fe1f6533871fc02cc
sopiandi <ekosopiandi.idris@gmail.com>
2023-07-14 16:33:01 +0800

    collaboration implemented (masih error)

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -68,7 +68,7 @@ class NotesHandler {
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
 
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
       const note = await this._service.getNoteById(id);
 
       return {
@@ -101,11 +101,10 @@ class NotesHandler {
   async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
-      const { title, body, tags } = request.payload;
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
 
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
       await this._service.editNoteById(id, request.payload);
 
       return {
